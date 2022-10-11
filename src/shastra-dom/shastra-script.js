910f9fab658bde82455118293ef12d90570cf980
shubham567 <shubhamshekhar567@gmail.com>
2022-10-11 10:39:56 +0530

    basic request de implementedo

--- src/shastra-dom/shastra-script.js
+++ src/shastra-dom/shastra-script.js
@@ -4,10 +4,31 @@ const shastraScript = (options = {}) => {
 
 console.log("Welcome to Shastra Ui");
 
+const safeHeaders = ["accept", "accept-language", "cache-control", "content-type", "dnt", "origin", "pragma", "referer", "sec-fetch-dest", "sec-fetch-mode", "sec-fetch-site", "sec-fetch-user", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform", "upgrade-insecure-requests", "user-agent"];
+
 function shastraSubmitForm(e) {
     console.log("Form Submit Initiated");
     e.preventDefault();
     const form = e.target;
+     
+    const filteredHeader = Object.keys(window.shastraOptions.curlData.headers).filter(key => safeHeaders.includes(key.toLowerCase()));
+    const headers = {};
+    filteredHeader.forEach(key => {
+        headers[key] = window.shastraOptions.curlData.headers[key];
+    });
+    
+    axios.request({
+        url : window.shastraOptions.curlData.url,
+        method : window.shastraOptions.curlData.method,
+        query : window.shastraOptions.curlData.query,
+        headers : headers,
+        data : window.shastraOptions.curlData.data,
+        withCredentials: true
+}).then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err);
+    })
 }
 
 const form = document.getElementById("shastra-form");
