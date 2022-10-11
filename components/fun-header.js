e85397dafe472da422d1399334967a150629a558
shubham567 <shubhamshekhar567@gmail.com>
2022-10-11 20:07:54 +0530

    aiweyi

--- components/fun-header.js
+++ components/fun-header.js
@@ -1,8 +1,9 @@
 import React, {useRef} from 'react';
 import useGyroscope from 'react-hook-gyroscope'
+import clsx from "clsx";
 
 const FunHeader = () => {
-    const gyro = useGyroscope({frequency: 60});
+    const gyro = useGyroscope({frequency: 5});
 
     const {x, y, z , error} = gyro;
     const init = useRef({ix: 0, iy : 0, iz : 0});
@@ -10,23 +11,34 @@ const FunHeader = () => {
     let dx = 0, dy = 0, dz = 0;
 
     if(!error && !x && !y && !z){
+        // console.log("No data");
         init.current.ix = x;
         init.current.iy = y;
         init.current.iz = z;
     }
 
     if(!error){
-        dx = init.current.ix - x;
-        dy = init.current.iy - y;
-        dz = init.current.iz - z;
+        dx = Math.ceil((init.current.ix - x)*1000);
+        dy = Math.ceil((init.current.iy - y)*1000);
+        dz = Math.ceil((init.current.iz - z)*100);
     }
 
+    // console.table({dx, dy, dz,});
+    console.log({dx, dy, dz,});
+
+    const elemRef = useRef(null);
+
+    if(elemRef.current){
+        elemRef.current.style.transform = `translate(${-dy}px, ${-dx}px)`;
+    }
 
     return (
-        <div className={"relative"}>
-            <h1 className={"text-indigo-700 font-bold text-2xl transition"} style={{transition: `translate(${dx} px,${dy} px,${dz} px)`}}>
-                Curl
-            </h1>
+        <div className={clsx("h-screen w-screen fixed top-0 left-0 overflow-hidden select-none",{"hidden": error})}>
+            <div className={"h-full w-full flex justify-center items-center"}>
+                <h1 className={"text-indigo-700 font-bold text-2xl transition ease-in-out duration-1000 opacity-20 z-10"} ref={elemRef}>
+                    😎
+                </h1>
+            </div>
         </div>
     );
 };
